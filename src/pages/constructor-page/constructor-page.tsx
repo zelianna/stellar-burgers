236e1661dd8b8a5cb1b5e9d613d7131fd4084f62
Anna Zelenskaia <anya034@yandex.ru>
2024-12-10 14:38:46 +0100

    Pr11_step1

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from '../../services/store';
 import { fetchIngredients } from '../../services/reducers/ingredientsReducer';
 import styles from './constructor-page.module.css';
 
-import { BurgerIngredients } from '../../components';
+import { BurgerIngredientsUI } from '../../components/ui/burger-ingredients/burger-ingredients';
 import { BurgerConstructor } from '../../components';
 import { Preloader } from '../../components/ui';
 import { FC } from 'react';
@@ -24,6 +24,35 @@ export const ConstructorPage: FC = () => {
   }, [dispatch]);
 
   //const isIngredientsLoading = false;
+  const buns = ingredients.filter((ingredient) => ingredient.type === 'bun');
+  const mains = ingredients.filter((ingredient) => ingredient.type === 'main');
+  const sauces = ingredients.filter(
+    (ingredient) => ingredient.type === 'sauce'
+  );
+
+  // Создание рефов для заголовков
+  const titleBunRef = useRef<HTMLHeadingElement>(null);
+  const titleMainRef = useRef<HTMLHeadingElement>(null);
+  const titleSaucesRef = useRef<HTMLHeadingElement>(null);
+
+  // Коллбэк-рефы для категорий
+  const bunsRef = (node?: Element | null) => {
+    if (node) {
+      console.log('Buns element:', node);
+    }
+  };
+
+  const mainsRef = (node?: Element | null) => {
+    if (node) {
+      console.log('Mains element:', node);
+    }
+  };
+
+  const saucesRef = (node?: Element | null) => {
+    if (node) {
+      console.log('Sauces element:', node);
+    }
+  };
 
   return (
     <>
@@ -37,7 +66,23 @@ export const ConstructorPage: FC = () => {
             Соберите бургер
           </h1>
           <div className={`${styles.main} pl-5 pr-5`}>
-            <BurgerIngredients />
+            {error ? (
+              <div className='error-message'>Ошибка загрузки: {error}</div>
+            ) : (
+              <BurgerIngredientsUI
+                currentTab='bun'
+                buns={buns}
+                mains={mains}
+                sauces={sauces}
+                titleBunRef={titleBunRef}
+                titleMainRef={titleMainRef}
+                titleSaucesRef={titleSaucesRef}
+                bunsRef={bunsRef}
+                mainsRef={mainsRef}
+                saucesRef={saucesRef}
+                onTabClick={(tab) => console.log(`Текущая вкладка: ${tab}`)}
+              />
+            )}
             <BurgerConstructor />
           </div>
         </main>
