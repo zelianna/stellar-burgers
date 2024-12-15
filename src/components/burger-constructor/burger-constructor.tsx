f883c07d732d675d56bddc020aed812ba6a915e6
Anna Zelenskaia <anya034@yandex.ru>
2024-12-15 12:27:36 +0100

    Pr11_перенаправление при оформл заказа

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,26 +1,39 @@
 import { FC, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { TConstructorIngredient } from '@utils-types';
+import { orderBurgerApi } from '../../utils/burger-api';
 import { BurgerConstructorUI } from '@ui';
-import { useSelector } from '../../services/store';
+import { useSelector, useDispatch } from '../../services/store';
+import { useState } from 'react';
 export const BurgerConstructor: FC = () => {
   /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   // Данные из стора
   const bun = useSelector((state) => state.burgerConstructor.bun);
   const ingredients = useSelector(
     (state) => state.burgerConstructor.ingredients
   );
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const [orderRequest, setOrderRequest] = useState(false);
+  const [orderModalData, setOrderModalData] = useState(null);
 
   const constructorItems = {
     bun,
     ingredients
   };
 
-  const orderRequest = false;
-
-  const orderModalData = null;
+  //const orderRequest = false;
+  //const orderModalData = null;
 
   const onOrderClick = () => {
+    if (!isAuthenticated) {
+      navigate('/login'); // Перенаправление на страницу входа
+      return;
+    }
+
     if (!constructorItems.bun || orderRequest) return;
   };
   const closeOrderModal = () => {};
